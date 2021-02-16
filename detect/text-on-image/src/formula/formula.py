c7171ab6ecd1db2860a24c4d8c535d63bb34ad84
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-02-16 09:36:38 -0300

    update rit text recognition README
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- detect/text-on-image/src/formula/formula.py
+++ detect/text-on-image/src/formula/formula.py
@@ -4,10 +4,9 @@ import pytesseract
 import os
 import re
 
-# List of available languages
-# print(pytesseract.get_languages(config=''))
-
 def run(image_path):
+
+    # Check if Tesseract is installed
     try:
         output = os.popen("which tesseract").read()
         tesseract_match = re.match("(.*?)(?=\\n)", output)
@@ -16,6 +15,7 @@ def run(image_path):
     except:
         print("You don't seem to have Tesseract installed. Have a look here: https://tesseract-ocr.github.io/tessdoc/Downloads.html")
 
+    # Use Pytesseract (a Tessaract warper) to extract Text from Image
     try:
         text = pytesseract.image_to_string(Image.open(image_path))
         print("\033[1m✅ Successfully convert the image into text:\033[0m")
@@ -24,3 +24,6 @@ def run(image_path):
         print("\033[1m❌ Couldn't converting the image into text:\033[0m")
         print("Please, check the image path and try again.")
         print("If the problem persists, open an ISSUE on https://github.com/GuillaumeFalourd/formulas-python")
+
+# List of available languages
+# print(pytesseract.get_languages(config=''))
--- text/recognition/README.md
+++ text/recognition/README.md
@@ -13,3 +13,5 @@ A formula that allows to detect text on image or through webcam.
 This formula gathered 2 formulas in one:
 - [Image Text Detection](https://github.com/GuillaumeFalourd/formulas-python/tree/master/detect/text-on-image): `rit detect image-to-text`
 - [Live Text Detection](https://github.com/GuillaumeFalourd/formulas-python/tree/master/detect/text-on-video): `rit detect text-on-video`
+
+When using the Webcam, type the `Q key` on the **Text Detection Window** to close it and end the formula execution.
